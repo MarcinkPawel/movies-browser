0c8192fea415ef0f47d41178296226f2e7fc7ae2
Gosia Magdzik <melassa26@gmail.com>
2023-04-19 17:04:04 +0200

    flex instead of grow

--- src/Tile/styled.js
+++ src/Tile/styled.js
@@ -4,14 +4,15 @@ export const Tile = styled.article`
     background: ${({ theme }) => theme.color.white};
     padding: 40px;
     box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-    display: grid;
-    grid-template-columns: auto, 1fr;
-    grid-gap: 40px;
+    display: flex;
+    flex-direction: row;
+    row-gap: 40px;
 `;
 
 export const Image = styled.img`
     width: 312px;
     height: 464px;
+    margin-right: 40px;
 `;
 
 export const Content = styled.div``;
