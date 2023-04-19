e63edfdb593bb4ec5cebf200e97b4dbb8921ada1
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2023-04-19 20:27:38 +0200

    colors, breakpoint and fontWeight added

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,17 @@
 export const theme = {
-    color: {
-        
-    },
-    breakpoint: {
-        
-    },
+  color: {
+    white: "#FFFFFF",
+    silver: "#C4C4C4",
+    waterloo: "#7E839A",
+    woodsmoke: "#18181B",
+  },
+  breakpoint: {
+    mobile: 767,
+  },
+  fontWeight: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+  },
 };
