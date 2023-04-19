65e9e179078714a0d2c335acae73ce49f4a41f73
Gosia Magdzik <melassa26@gmail.com>
2023-04-19 16:05:44 +0200

    colors added

--- src/theme.js
+++ src/theme.js
@@ -2,8 +2,12 @@ export const theme = {
     color: {
         white: "#FFFFFF",
         mercury: "#E5E5E5",     
+        candlelight: "#FCD420",
+        mystic: "#E4E6F0",
+        black: "#000000",
+        stormGray: "#74788B",
     },
 
-    breakpoint: {
-        
-    };
+    breakpoint: {   
+}
+};
