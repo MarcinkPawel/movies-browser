3bbeb387c57828379e52524617bfe68eee86442e
Gosia Magdzik <melassa26@gmail.com>
2023-04-21 16:47:42 +0200

    Title, Img and List addes

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
 import { MainTileDetails } from "./Tile/index.js";
-import { MovieContainer } from "./Container";
-import { Person } from "./features/personTail";
-import React from "react";
-import MovieTile from "./components/MovieTile";
+//import { Person } from "./features/personTail";
+//import MovieTile from "./components/MovieTile";
 import { HashRouter } from "react-router-dom";
-import { Nav } from "./styled";
+import { Nav, Title, Video, List,  } from "./styled";
+import video from "./images/video.svg";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle /> 
       <HashRouter>
         <Nav>
-          <GlobalStyle />
-            <MovieContainer>
-              <MovieTile />
-              <Person />
-              <MainTileDetails />
-            </MovieContainer>
+          <Title>
+            <Video src={video}/>
+            Movies Browser
+          </Title>
+          <List>
+
+          </List>
+ 
         </Nav>
       </HashRouter>
     </ThemeProvider>
--- src/styled.js
+++ src/styled.js
@@ -4,4 +4,27 @@ export const Nav = styled.nav`
     background: ${({ theme }) => theme.color.black};
     color: ${({ theme }) => theme.color.white};
     height: 94px;
+    max-width: 1920px;
+    margin: 0%, auto;
+    display: flex;
+    align-items: center;
 `;
+
+export const Title = styled.p`
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 40px;
+    letter-spacing: -1.5px;
+    margin-left: 52px;
+    text-transform: capitalize;
+    display: flex;
+    align-items: center;
+`;
+
+export const Video = styled.img`
+    align-items: center;
+    width: 40px;
+    margin-right: 12px;
+`;
+
+export const List = styled.ul``;
