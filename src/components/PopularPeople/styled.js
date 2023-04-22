7f46433b505bfdbe9c8139b9706d86a885ed7051
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2023-04-22 15:48:44 +0200

    Popluar people component added

---
+++ src/components/PopularPeople/styled.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 1368px;
+  background: ${({ theme }) => theme.color.mercury};
+  margin: 40px auto 0;
+  padding: 16px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    max-width: 320px;
+    width: 100%;
+    margin: 24px auto;
+  }
+`;
+
+export const Header = styled.h1`
+  font-weight: 600;
+  font-size: 36px;
+  color: ${({ theme }) => theme.color.black};
+  line-height: 1.2;
+  margin-bottom: 24px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 18px;
+    margin-bottom: 12px;
+  }
+`;
+export const MoviesContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  grid-column-gap: 24px;
+  grid-row-gap: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-column-gap: 16px;
+    grid-row-gap: 16px;
+  }
+`;
