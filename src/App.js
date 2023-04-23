6c9b39b8c682a7eb8e3b070eec8c5d4aa6712d8d
Gosia Magdzik <melassa26@gmail.com>
2023-04-23 20:50:35 +0200

    app.js changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
-import { MainTileDetails } from "./Tile/index.js";
+//import { MainTileDetails } from "./Tile/index.js";
 //import { Person } from "./features/personTail";
 //import MovieTile from "./components/MovieTile";
 import { HashRouter } from "react-router-dom";
 import { Nav, Title, Video, List, Li, Search, Magnifier, InputStyled } from "./styled";
 import video from "./images/video.svg";
 import magnifier from "./images/search.svg";
+//import { Error } from "./Error";
+import { Loader } from "./Loader"
 
 function App() {
   return (
@@ -31,6 +33,7 @@ function App() {
           </Search>
         </Nav>
       </HashRouter>
+      <Loader/>
     </ThemeProvider>
   );
 }
