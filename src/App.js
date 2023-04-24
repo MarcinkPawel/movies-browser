29a00fa3f6d22cda4b03d5bdf2447b7e1c166635
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2023-04-24 21:20:27 +0200

    React rouder  is working, page is rendering,

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import { toMovie, toPerson, toPopularMovies, toPopularPeople } from "./routes";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Nav } from "./components/Nav/styled";
 import { MovieTile } from "./components/MovieTile";
 import { Person } from "./components/PersonTile";
@@ -16,10 +15,10 @@ export default () => (
         <HashRouter>
           <Nav />
           <Routes>
-            <Route path={toMovie} element={<MovieTile />} />
-            <Route path={toPerson} element={<Person />} />
-            <Route path={toPopularMovies} element={<Movies />} />
-            <Route path={toPopularPeople} element={<People />} />
+            <Route path="/movie" element={<MovieTile />} />
+            <Route path="/person" element={<Person />} />
+            <Route path="/popular-movies" element={<Movies />} />
+            <Route path="/popular-people" element={<People />} />
             <Route path="/" element={<Movies />} />
           </Routes>
         </HashRouter>
