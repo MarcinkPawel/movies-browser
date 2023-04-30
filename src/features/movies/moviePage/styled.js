6db1d9ccaa14ab0dca23e7626fca82f9369fdbb9
Gosia Magdzik <melassa26@gmail.com>
2023-04-30 13:34:41 +0200

    Links added

--- src/features/movies/moviePage/styled.js
+++ src/features/movies/moviePage/styled.js
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const TileLink = styled(Link)`
+  text-decoration: none;
+`;
 
 export const Tile = styled.article`
   background: ${({ theme }) => theme.color.white};
