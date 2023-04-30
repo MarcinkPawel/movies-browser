a742508e1cefd067cf05ed7aefb7d20401c493c1
Gosia Magdzik <melassa26@gmail.com>
2023-04-30 13:35:31 +0200

    temporary routes added

--- src/features/movies/moviePage/MoviesList/index.js
+++ src/features/movies/moviePage/MoviesList/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { MovieTile } from "..";
-import { Header, MoviesContainer, Wrapper } from "./styled";
+import { Header, MoviesContainer, Wrapper, MovieLink } from "./styled";
 
 export const Movies = () => {
   return (
     <Wrapper>
       <Header>Popular movies</Header>
       <MoviesContainer>
-      <MovieTile />
-      <MovieTile />
-      <MovieTile />
-      <MovieTile />
-      <MovieTile />
-      <MovieTile />
-      <MovieTile />
-      <MovieTile />
-
+          <MovieLink to={"/movie"}> <MovieTile /> </MovieLink>
+          <MovieLink to={"/movie"}> <MovieTile /> </MovieLink>
+          <MovieLink to={"/movie"}> <MovieTile /> </MovieLink>
+          <MovieLink to={"/movie"}> <MovieTile /> </MovieLink>
+          <MovieLink to={"/movie"}> <MovieTile /> </MovieLink>
+          <MovieLink to={"/movie"}> <MovieTile /> </MovieLink>
+          <MovieLink to={"/movie"}> <MovieTile /> </MovieLink>
+          <MovieLink to={"/movie"}> <MovieTile /> </MovieLink>
+          <MovieLink to={"/movie"}> <MovieTile /> </MovieLink>
       </MoviesContainer>
     </Wrapper>
   );
--- src/features/people/peoplePage/index.js
+++ src/features/people/peoplePage/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import { Person } from "../personPage";
-import { Header, PeopleContainer, Wrapper } from "./styled";
+import { Header, 
+        PeopleContainer, 
+        Wrapper,
+        PersonLink } from "./styled";
 
 export const People = () => {
   return (
     <Wrapper>
       <Header>Popular people</Header>
       <PeopleContainer>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <PersonLink to={"/person"} ><Person /></PersonLink>
+        <PersonLink to={"/person"} ><Person /></PersonLink>
+        <PersonLink to={"/person"} ><Person /></PersonLink>
+        <PersonLink to={"/person"} ><Person /></PersonLink>
+        <PersonLink to={"/person"} ><Person /></PersonLink>
+        <PersonLink to={"/person"} ><Person /></PersonLink>
+        <PersonLink to={"/person"} ><Person /></PersonLink>
+        <PersonLink to={"/person"} ><Person /></PersonLink>
+        <PersonLink to={"/person"} ><Person /></PersonLink>
+        <PersonLink to={"/person"} ><Person /></PersonLink>
+        <PersonLink to={"/person"} ><Person /></PersonLink>
       </PeopleContainer>
     </Wrapper>
   );
