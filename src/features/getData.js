62f76189665030f76a1b473e4fdb32f0d2f1478a
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2023-05-09 21:28:43 +0200

    Setting store and getData

---
+++ src/features/getData.js
@@ -0,0 +1,11 @@
+export const key = "79dae1642f2133ccb8691353190ab37b";
+export const searchAPI = "https://api.themoviedb.org/3/search"
+export const popularMoviesAPI = "https://api.themoviedb.org/3/movie/popular";
+export const popularPeopleAPI = "https://api.themoviedb.org/3/person/popular";
+export const imagesAPIw200 = "https://image.tmdb.org/t/p/w200/";
+export const imagesAPI300x450 = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
+export const imagesAPIw400 = "https://image.tmdb.org/t/p/w400/";
+export const imagesAPI600x900 = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/";
+export const imagesFullApi = "https://image.tmdb.org/t/p";
+export const movieByIDAPI = "https://api.themoviedb.org/3/movie/";
+export const personAPI = "https://api.themoviedb.org/3/person/";
