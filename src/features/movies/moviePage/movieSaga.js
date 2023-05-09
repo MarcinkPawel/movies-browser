1f527ac3df11456cedf12c67deb6d7649b66d6d3
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2023-05-09 21:29:37 +0200

    Movie slice and saga done

---
+++ src/features/movies/moviePage/movieSaga.js
@@ -0,0 +1,24 @@
+import { call, delay, put, takeLatest } from "redux-saga/effects"
+import { getCrewByID, getMovieByID } from "./getMovieById";
+import { fetchMovieById, fetchMovieByIdError, fetchMovieByIdSuccess } from "./movieSlice";
+
+function* fetchMovieByIDHandler({ payload }) {
+  try {
+    const movieDescription = yield call(getMovieByID, payload);
+    const movieCrew = yield call (getCrewByID, payload);
+    const movieInfo = {
+      movieDescription: movieDescription,  
+      movieCrew: movieCrew
+    };
+    yield delay(300);
+    yield put(fetchMovieByIdSuccess(movieInfo));
+  }
+  catch (error) {
+    yield put(fetchMovieByIdError());
+    yield call(alert, "Download failed, please try again or check your internet connection")
+  }
+};
+
+export function* movieSaga() {
+  yield takeLatest(fetchMovieById, fetchMovieByIDHandler);
+};
