2ff7788fc7f97536ae81d16b259f0aee0887811f
Gosia Magdzik <melassa26@gmail.com>
2023-05-11 15:07:19 +0200

    Search Result small change

--- src/common/SearchResult/index.js
+++ src/common/SearchResult/index.js
@@ -1,10 +1,10 @@
 import { Container, SearchInfo, ResultList } from "./styled";
 import React from "react";
 
-export const SearchResult = () => {
+export const SearchResult = ({ title }) => {
   return (
     <Container>
-      <SearchInfo>Search results for ""</SearchInfo>
+      <SearchInfo> Search results for{title}</SearchInfo>
       <ResultList>
 
       </ResultList>
