c2bc4ab033288187a41632660cd2cf64755c636a
Marta Żebrowska-Giers <115993282+marta-zebrowska@users.noreply.github.com>
2023-05-15 09:46:01 +0200

    SearchInput changed

--- src/common/Nav/SearchInput/index.js
+++ src/common/Nav/SearchInput/index.js
@@ -1,10 +1,15 @@
-import { SearchStyled, InputStyled, Magnifier } from "./styled"
+import React from 'react';
+import { SearchStyled, InputStyled, Magnifier } from "./styled";
 import magnifier from "../../../images/search.svg";
 import { useLocation } from "react-router-dom";
-import { useQueryParameters,
-         useReplaceQueryParameters } from "../../../features/search/queryParameters";
-import { pageQueryParamName,
-         searchQueryParamName, } from "../../../features/search/queryParamNames";
+import {
+  useQueryParameters,
+  useReplaceQueryParameters,
+} from "../../../features/search/queryParameters";
+import {
+  pageQueryParamName,
+  searchQueryParamName,
+} from "../../../features/search/queryParamNames";
 import { Results } from "../Results";
 import { selectOpen } from "../../../features/search/searchSlice";
 import { useSelector } from "react-redux";
@@ -14,35 +19,29 @@ export const Search = () => {
   const query = useQueryParameters(searchQueryParamName);
   const isOpen = useSelector(selectOpen);
   const replaceQueryParameters = useReplaceQueryParameters();
-
   const onInputChange = ({ target }) => {
     replaceQueryParameters([
-          {
-          key: searchQueryParamName,
-          value: target.value.trim()  !== "" 
-                                      ? target.value : undefined,
-          },
-          { key: pageQueryParamName },
+      {
+        key: searchQueryParamName,
+        value: target.value.trim() !== "" ? target.value : undefined,
+      },
+      { key: pageQueryParamName },
     ]);
   };
 
-    return (
-        <SearchStyled>
-          <Magnifier src={magnifier} />
-          <InputStyled 
-            placeholder={`Search for ${
-              location.pathname.includes("movies") 
-                                        ? "movies..." 
-                                        : "people..."
-            }`}
-            value={query}
-            onChange={onInputChange}
-          />
-          {(query && isOpen) &&
-          <Results query={query} />
-          }
-        </SearchStyled>
-    );
+  return (
+    <SearchStyled>
+      <Magnifier src={magnifier} />
+      <InputStyled
+        placeholder={`Search for ${
+          location.pathname.includes("movies") ? "movies..." : "people..."
+        }`}
+        value={query ? query : ""}
+        onChange={onInputChange}
+      />
+      {query && isOpen && <Results query={query} />}
+    </SearchStyled>
+  );
 };
 
 //Results Component jest jako test czy to w ogóle się odpali
