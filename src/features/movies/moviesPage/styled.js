f6603dc5e9d303b88673554c8b00428170ed62f0
Gosia Magdzik <melassa26@gmail.com>
2023-05-16 18:21:31 +0200

    New List for movie details

--- src/features/movies/moviesPage/styled.js
+++ src/features/movies/moviesPage/styled.js
@@ -13,3 +13,23 @@ export const Wrapper = styled.div`
     justify-content: center;
   }
 `;
+
+export const List = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 24px;
+  margin: 24px auto 40px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 18px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-column-gap: 16px;
+  }
+`;
+
